ebb6eeb5ac535579e0db5ea2de2af3541c8e9d56
❥ giulia <giuliamferreira01@gmail.com>
2025-01-25 17:03:22 -0300

    Update 008.sh

--- submission/008.sh
+++ submission/008.sh
@@ -4,7 +4,7 @@
 #!/bin/bash
 txid="e5969add849689854ac7f28e45628b89f7454b83e9699e551ce14b6f90c86163"
 
-tx=$(bitcoin-core.cli getrawtransaction $txid true)
+tx=$(bitcoin-cli getrawtransaction $txid true)
 
 pubkey=$(echo $tx | jq -r .vin[0].txinwitness[2][4:70])
 echo $pubkey
