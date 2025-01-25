b63f723653dd80029b441a14d75121f5aff98402
❥ giulia <giuliamferreira01@gmail.com>
2025-01-25 17:34:14 -0300

    Update 007.sh

--- submission/007.sh
+++ submission/007.sh
@@ -2,23 +2,26 @@
 
 #!/bin/bash
 
-block_hash = $(bitcoin-cli getblockhash 123321)
-readarray -t transact_list < <(bitcoin-cli getblock "$block_hash" | jq -r '.tx[]')
+block_hash=$(bitcoin-cli getblockhash 123321)
+readarray -t tx_list < <(bitcoin-cli getblock $block_hash | jq -r .tx[])
 
-for transact_id in "${transact_list[@]}"
-do
-        raw_transact=$(bitcoin-cli getrawtransaction "$transact_id" true | jq -c .)
-        readarray -t output_list < <(echo "$raw_transact" | jq -c '.vout[]')
+for transact_id in "${tx_list[@]}"
+do  
+    rawtx=$(bitcoin-cli getrawtransaction "$transact_id" true | jq -c .)   
+    readarray -t outputs < <(echo "$rawtx" | jq -c .vout[])
 
-        for output_entry in "${output_list[@]}"
-        do
-                output_idx=$(echo "$output_entry" | jq -r '.n')
-                utxo=$(bitcoin-cli gettxout "$transact_id" "$output_idx")
+    for output in "${outputs[@]}"
+    do
+        index=$(echo $output | jq -r .n)
+        utxo=$(bitcoin-cli gettxout $transact_id $index)
 
-                if [[ $utxo ]] 
-                then
-                        output_address=$(echo "$utxo" | jq -r '.scriptPubKey.address')
-                        echo "$output_address"
-                fi
-        done
+        if [[ $utxo ]]
+        then
+            address=$(echo $utxo | jq -r .scriptPubKey.address)
+            echo $address
+            
+        fi
+        
+    done
+    
 done
