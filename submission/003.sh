9f92d7bd71df42a95aa5d4a3b4e9c515138ccdef
❥ giulia <giuliamferreira01@gmail.com>
2025-01-25 14:06:41 -0300

    Update 003.sh

--- submission/003.sh
+++ submission/003.sh
@@ -1,15 +1,15 @@
 # How many new outputs were created by block 123,456?
 
 #!/bin/bash
-block_hash=$(bitcoin-core.cli getblockhash 123456)
+block_hash=$(bitcoin-cli getblockhash 123456)
 
-block=$(bitcoin-core.cli getblock $block_hash true)
+block=$(bitcoin-cli getblock $block_hash true)
 transactions=$(echo $block | jq -r '.tx[]')
 
 total_outputs=0
 
 for txid in $transactions; do
-	raw_tx=$(bitcoin-core.cli getrawtransaction $txid true)
+	raw_tx=$(bitcoin-cli getrawtransaction $txid true)
 	outputs=$(echo $raw_tx | jq '.vout | length')
 	total_outputs=$((total_outputs + outputs))
 done
