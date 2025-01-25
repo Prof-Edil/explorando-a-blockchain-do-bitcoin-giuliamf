1812776f37f7677019fd1e6e10a272448d04215c
❥ giulia <giuliamferreira01@gmail.com>
2025-01-25 14:32:11 -0300

    Update 004.sh

--- submission/004.sh
+++ submission/004.sh
@@ -2,10 +2,11 @@
 #   `xpub6Cx5tvq6nACSLJdra1A6WjqTo1SgeUZRFqsX5ysEtVBMwhCCRa4kfgFqaT2o1kwL3esB1PsYr3CUdfRZYfLHJunNWUABKftK2NjHUtzDms2`
 
 #!/bin/bash
-xpub="xpub6Cx5tvq6nACSLJdra1A6WjqTo1SgeUZRFqsX5ysEtVBMwhCCRa4kfgFqaT2o1kwL3esB1PsYr3CUdfRZ>
+set -e
 
+xpub="xpub6Cx5tvq6nACSLJdra1A6WjqTo1SgeUZRFqsX5ysEtVBMwhCCRa4kfgFqaT2o1kwL3esB1PsYr3CUdfRZYfLHJunNWUABKftK2NjHUtzDms2"
 descriptor="tr(\$xpub/0/100)"
-descriptor_with_checksum=$(bitcoin-core.cli getdescriptorinfo "$descriptor" | jq -r '.desc>
 
-address=$(bitcoin-core.cli deriveaddresses "$descriptor_with_checksum")
+address=$(bitcoin-cli deriveaddresses "$descriptor")
+
 echo $address
