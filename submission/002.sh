2fc1abf1764abd99bad0aad139b8a4a5fac5bcb6
❥ giulia <giuliamferreira01@gmail.com>
2025-01-25 14:06:07 -0300

    Update 002.sh

--- submission/002.sh
+++ submission/002.sh
@@ -4,6 +4,6 @@
 #   signature: `HCsBcgB+Wcm8kOGMH8IpNeg0H4gjCrlqwDf/GlSXphZGBYxm0QkKEPhh9DTJRp2IDNUhVr0FhP9qCqo2W0recNM=`
 
 #!/bin/bash
-bitcoin-core.cli verifymessage "1E9YwDtYf9R29ekNAfbV7MvB4LNv7v3fGa" \
+bitcoin-cli verifymessage "1E9YwDtYf9R29ekNAfbV7MvB4LNv7v3fGa" \
   "HCsBcgB+Wcm8kOGMH8IpNeg0H4gjCrlqwDf/GlSXphZGBYxm0QkKEPhh9DTJRp2IDNUhVr0FhP9qCqo2W0recNM=" \
   "1E9YwDtYf9R29ekNAfbV7MvB4LNv7v3fGa"
