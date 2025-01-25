768827fd4fdf9de79cdb0e89b1f19120c862cc8a
❥ giulia <giuliamferreira01@gmail.com>
2025-01-25 14:21:04 -0300

    Update 004.sh

--- submission/004.sh
+++ submission/004.sh
@@ -2,8 +2,8 @@
 #   `xpub6Cx5tvq6nACSLJdra1A6WjqTo1SgeUZRFqsX5ysEtVBMwhCCRa4kfgFqaT2o1kwL3esB1PsYr3CUdfRZYfLHJunNWUABKftK2NjHUtzDms2`
 
 #!/bin/bash
+set -e
 xpub="xpub6Cx5tvq6nACSLJdra1A6WjqTo1SgeUZRFqsX5ysEtVBMwhCCRa4kfgFqaT2o1kwL3esB1PsYr3CUdfRZYfLHJunNWUABKftK2NjHUtzDms2"
-
 address=$(bitcoin-cli deriveaddresses "tr($xpub/0/100)" 0)
 
 echo $address
