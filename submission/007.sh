b040879bdc0d6896a7bedc8b2bbd61e8bfa7defb
❥ giulia <giuliamferreira01@gmail.com>
2025-01-25 17:18:18 -0300

    Update 007.sh

--- submission/007.sh
+++ submission/007.sh
@@ -5,15 +5,18 @@
 block_hash = $(bitcoin-cli getblockhash 123321)
 readarray -t transact_list < <(bitcoin-cli getblock "$block_hash" | jq -r '.tx[]')
 
-for transact_id in "${transact_list[@]}"; do
+for transact_id in "${transact_list[@]}"
+do
         raw_transact=$(bitcoin-cli getrawtransaction "$transact_id" true | jq -c .)
         readarray -t output_list < <(echo "$raw_transact" | jq -c '.vout[]')
 
-        for output_entry in "${output_list[@]}"; do
+        for output_entry in "${output_list[@]}"
+        do
                 output_idx=$(echo "$output_entry" | jq -r '.n')
                 utxo=$(bitcoin-cli gettxout "$transact_id" "$output_idx")
 
-                if [[ $utxo ]] then
+                if [[ $utxo ]] 
+                then
                         output_address=$(echo "$utxo" | jq -r '.scriptPubKey.address')
                         echo "$output_address"
                 fi
